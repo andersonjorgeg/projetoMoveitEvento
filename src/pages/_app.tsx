93c06daa2a7401c35671463517bc5b5b0cb25197
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-25 15:21:51 -0300

    arquivo do projeto moveit da NLW dia 4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,14 @@ import { useState } from 'react';
 import '../styles/global.css'
 
 import {ChallengesProvider} from '../contexts/ChallengesContext'
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 
 function MyApp({ Component, pageProps }) {
   return ( 
     <ChallengesProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ChallengesProvider>
-    
   )
 }
 
